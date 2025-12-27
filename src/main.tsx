39e157738c949b37f45dfdc92c71b250fcefcddd
Muhib123123 <muhibistanbuly2@gmail.com>
2025-12-27 21:16:50 +0100

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,12 @@ import "./i18n";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
+// Handle 404.html redirect
+if (window.location.pathname.includes('?p=')) {
+  const path = '/' + new URLSearchParams(window.location.search).get('p');
+  window.history.replaceState(null, '', window.location.pathname.slice(0, window.location.pathname.lastIndexOf('/')) + path + window.location.hash);
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
