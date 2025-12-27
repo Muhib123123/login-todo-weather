5da8786fd1dd8941b5fcf57816d744b8a9bc78fd
Muhib123123 <muhibistanbuly2@gmail.com>
2025-12-27 21:36:44 +0100

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import "./i18n";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
-// Handle 404.html redirect - restore the original path
-const params = new URLSearchParams(window.location.search);
-const redirect = params.get('p');
-if (redirect) {
-  window.history.replaceState(null, '', '/login-todo-weather/' + redirect);
-}
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/login-todo-weather">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 );
